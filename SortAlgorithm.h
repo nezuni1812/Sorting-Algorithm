02d78b612560f76be25a95ea5b0f2fd7ad3405f7
PhKhang <trannguyenphuckhang12@gmail.com>
2023-07-19 20:41:46 +0700

    Add: FlashSort, renamed some functions

--- SortAlgorithm.h
+++ SortAlgorithm.h
@@ -94,7 +94,7 @@ void QuickComp() {
 
 }
 
-void CoutingSort(int a[], int n){
+void Couting(int a[], int n){
     int maxVal = a[0];
     for (int i = 0; i < n; i++)
         if (maxVal < a[i])
@@ -120,7 +120,7 @@ void CoutingSort(int a[], int n){
         a[i] = b[i];
 }
 
-void RadixLSDSort(int a[], int n){
+void RadixLSD(int a[], int n){
     int maxVal = a[0];
     
     for (int i = 1; i < n; i++)
@@ -165,6 +165,54 @@ void RadixLSDSort(int a[], int n){
     
 }
 
-void FlashComp() {
-
+void FlashComp(int a[], int n) {
+    int minVal = a[0];
+	int max = 0;
+	int m = int(0.45 * n);
+	int* l = new int[m];
+	for (int i = 0; i < m; i++)
+		l[i] = 0;
+	for (int i = 1; i < n; i++)
+	{
+		if (a[i] < minVal)
+			minVal = a[i];
+		if (a[i] > a[max])
+			max = i;
+	}
+	if (a[max] == minVal)
+		return;
+	double c1 = (double)(m - 1) / (a[max] - minVal);
+	for (int i = 0; i < n; i++)
+	{
+		int k = int(c1 * (a[i] - minVal));
+		++l[k];
+	}
+	for (int i = 1; i < m; i++)
+		l[i] += l[i - 1];
+	swap(a[max], a[0]);
+	int nmove = 0;
+	int j = 0;
+	int k = m - 1;
+	int t = 0;
+	int flash;
+	while (nmove < n - 1)
+	{
+		while (j > l[k] - 1)
+		{
+			j++;
+			k = int(c1*(a[j] - minVal));
+		}
+		flash = a[j];
+		if (k < 0) break;
+		while (j != l[k])
+		{
+			k = int(c1*(flash - minVal));
+			int hold = a[t = --l[k]];
+			a[t] = flash;
+			flash = hold;
+			++nmove;
+		}
+	}
+	delete[] l;
+	Insertion(a, n);
 }
