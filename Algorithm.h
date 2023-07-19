3462bd80d895f3419767e843db7a730ff96a7b84
nezuni1812 <tranngocuyennhi1812@gmail.com>
2023-07-19 20:16:12 +0700

    Update Algorithm.h

--- Algorithm.h
+++ Algorithm.h
@@ -6,16 +6,30 @@ void SelectionSort() {
 
 }
 
-void SelectionSortComp() {
-
+void SelectionSortComp(int a[], int n, unsigned long long &compare){
+    for (int i = 0; ++compare && i < n - 1; ++i) {
+        int minIndex = i;
+        for (int j = i + 1; ++compare && j < n; ++j) 
+            if (++compare && a[j] < a[minIndex]) 
+                minIndex = j;
+        swap(a[i], a[minIndex]);
+    }
 }
 
 void InsertionSort() {
 
 }
 
-void InsertionSortComp() {
-
+void InsertionSortComp(int a[], int n, unsigned long long &compare){
+    for (int i = 1; ++compare && i < n; ++i) {
+        int key = a[i];
+        int j = i - 1;
+        while (++compare && j >= 0 && ++compare && a[j] > key) {
+            a[j + 1] = a[j];
+            j = j - 1;
+        }
+        a[j + 1] = key;
+    }
 }
 
 void BubbleSort() {
