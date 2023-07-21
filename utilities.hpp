cd201c453f4f98245dc563cc122cf0496e9b0e81
nezuni1812 <tranngocuyennhi1812@gmail.com>
2023-07-21 21:25:44 +0700

    Update utilities.hpp

--- utilities.hpp
+++ utilities.hpp
@@ -267,16 +267,16 @@ void AlgorithmMode(int argc, char* argv[]){
                         writeTo("input_1.txt", arr, aSize);
                         break;
                     case 1:
-                        cout << "Nearly Sorted\n";
-                        writeTo("input_2.txt", arr, aSize);
-                        break;
-                    case 2:
                         cout << "Sorted\n";
                         writeTo("input_3.txt", arr, aSize);
                         break;
-                    case 3:
+                    case 2:
                         cout << "Reversed Sorted\n";
                         writeTo("input_4.txt", arr, aSize);
+                        break;
+                    case 3:
+                        cout << "Nearly Sorted\n";
+                        writeTo("input_2.txt", arr, aSize);
                         break;
                 }
                 cout << DIVIDER;
@@ -315,11 +315,11 @@ void AlgorithmMode(int argc, char* argv[]){
             int order = 0;
             if (strcmp(argv[4], "-rand") == 0)
                 order = 0;
-            if (strcmp(argv[4], "-nsorted") == 0)
-                order = 1;
             if (strcmp(argv[4], "-sorted") == 0)
-                order = 2;
+                order = 1;
             if (strcmp(argv[4], "-rev") == 0)
+                order = 2;
+            if (strcmp(argv[4], "-nsorted") == 0)
                 order = 3;
             
             cout << DIVIDER;
@@ -426,11 +426,11 @@ void ComparisonMode(int argc, char* argv[]) {
         int order = 0;
         if (strcmp(argv[5], "-rand") == 0)
             order = 0;
-        if (strcmp(argv[5], "-nsorted") == 0)
-            order = 1;
         if (strcmp(argv[5], "-sorted") == 0)
-            order = 2;
+            order = 1;
         if (strcmp(argv[5], "-rev") == 0)
+            order = 2;
+        if (strcmp(argv[5], "-nsorted") == 0)
             order = 3;
 
         cout << "Input size: " << n << "\n";
@@ -440,13 +440,13 @@ void ComparisonMode(int argc, char* argv[]) {
                 cout << "Randomize\n";
                 break;
             case 1:
-                cout << "Nearly Sorted\n";
-                break;
-            case 2:
                 cout << "Sorted\n";
                 break;
-            case 3:
+            case 2:
                 cout << "Reversed Sorted\n";
+                break;
+            case 3:
+                cout << "Nearly Sorted\n";
                 break;
         }
         
