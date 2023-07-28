e53eee5783959276d176071ffd4522761b30e3d1
PhKhang <trannguyenphuckhang12@gmail.com>
2023-07-28 21:09:43 +0700

    Update: moving to SOURCE folder

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-g++ main.cpp -o main
+g++ SOURCE/main.cpp -o main
 if %errorlevel% equ 0 (
     echo Program compiled successfully!
     echo Use this command to test it out: .\main.exe -a selection-sort 50000 -both
